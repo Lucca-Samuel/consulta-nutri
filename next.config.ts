540c81c1c09e58c60f82df3e3a82042c900cf738
Lucca-Samuel <samuellucca1002@gmail.com>
2025-03-17 15:32:46 -0300

    feat: first commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["via.placeholder.com", "https://placehold.co"], // Adicione o domínio aqui
+  },
+};
+
+export default nextConfig;
