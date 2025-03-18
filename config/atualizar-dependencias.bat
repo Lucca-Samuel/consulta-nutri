0200857cab00040c48e697a47edeef63dca880a8
Lucca-Samuel <samuellucca1002@gmail.com>
2025-03-18 16:46:42 -0300

    build(atualizacao): atualizações

---
+++ config/atualizar-dependencias.bat
@@ -0,0 +1,77 @@
+@REM @echo off
+@REM npm.cmd install @emotion/react@11.14.0
+@REM npm.cmd install @emotion/styled@11.14.0
+@REM npm.cmd install @fontsource/roboto@5.2.5
+@REM npm.cmd install @mui/icons-material@6.4.8
+@REM npm.cmd install @mui/material@6.4.8
+@REM npm.cmd install @radix-ui/react-dropdown-menu@2.1.6
+@REM npm.cmd install @radix-ui/react-menubar@1.1.6
+@REM npm.cmd install @radix-ui/react-navigation-menu@1.2.5
+@REM npm.cmd install @radix-ui/react-slot@1.1.2
+@REM npm.cmd install @types/node@20.17.24
+@REM npm.cmd install @types/react@18.3.18
+@REM npm.cmd install @types/react-dom@18.3.5
+@REM npm.cmd install class-variance-authority@0.7.1
+@REM npm.cmd install date-fns@4.1.0
+@REM npm.cmd install embla-carousel@8.5.2
+@REM npm.cmd install embla-carousel-autoplay@8.5.2
+@REM npm.cmd install embla-carousel-react@8.5.2
+@REM npm.cmd install eslint@9.22.0
+@REM npm.cmd install eslint-config-next@15.2.3
+@REM npm.cmd install lint-staged@15.5.0
+@REM npm.cmd install lucide@0.483.0
+@REM npm.cmd install lucide-react@0.483.0
+@REM npm.cmd install next@15.2.3
+@REM npm.cmd install postcss@8.5.3
+@REM npm.cmd install prettier@3.5.3
+@REM npm.cmd install prettier-plugin-tailwindcss@0.6.11
+@REM npm.cmd install react@19.0.0
+@REM npm.cmd install react-bootstrap@2.10.9
+@REM npm.cmd install react-day-picker@9.6.2
+@REM npm.cmd install react-dom@19.0.0
+@REM npm.cmd install react-icons@5.5.0
+@REM npm.cmd install react-router-dom@7.3.0
+@REM npm.cmd install swiper@11.2.5
+@REM npm.cmd install tailwind-merge@3.0.2
+@REM npm.cmd install tailwindcss@4.0.14
+@REM npm.cmd install typescript@5.8.2
+@REM npm.cmd install uuid@latest
+
+@echo off
+npm.cmd install @emotion/react@latest
+npm.cmd install @emotion/styled@latest
+npm.cmd install @fontsource/roboto@latest
+npm.cmd install @mui/icons-material@latest
+npm.cmd install @mui/material@latest
+npm.cmd install @radix-ui/react-dropdown-menu@latest
+npm.cmd install @radix-ui/react-menubar@latest
+npm.cmd install @radix-ui/react-navigation-menu@latest
+npm.cmd install @radix-ui/react-slot@latest
+npm.cmd install @types/node@latest
+npm.cmd install @types/react@latest
+npm.cmd install @types/react-dom@latest
+npm.cmd install class-variance-authority@latest
+npm.cmd install date-fns@latest
+npm.cmd install embla-carousel@latest
+npm.cmd install embla-carousel-autoplay@latest
+npm.cmd install embla-carousel-react@latest
+npm.cmd install eslint@latest
+npm.cmd install eslint-config-next@latest
+npm.cmd install lint-staged@latest
+npm.cmd install lucide@latest
+npm.cmd install lucide-react@latest
+npm.cmd install next@latest
+npm.cmd install postcss@latest
+npm.cmd install prettier@latest
+npm.cmd install prettier-plugin-tailwindcss@latest
+npm.cmd install react@latest
+npm.cmd install react-bootstrap@latest
+npm.cmd install react-day-picker@latest
+npm.cmd install react-dom@latest
+npm.cmd install react-icons@latest
+npm.cmd install react-router-dom@latest
+npm.cmd install swiper@latest
+npm.cmd install tailwind-merge@latest
+npm.cmd install tailwindcss@latest
+npm.cmd install typescript@latest
+npm.cmd install uuid@latest
