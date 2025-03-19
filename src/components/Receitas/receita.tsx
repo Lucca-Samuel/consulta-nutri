2b00aa82ac4204bd98aa24b925d645f04be7c945
Lucca-Samuel <samuellucca1002@gmail.com>
2025-03-19 14:11:58 -0300

    fix(button property): propriedade do botao undefinida

--- src/components/Receitas/receita.tsx
+++ src/components/Receitas/receita.tsx
@@ -5,7 +5,10 @@ import "./style.css";
 
 const Receitas = () => {
   return (
-    <div className="container-xl flex flex-col items-center py-10" id="receitas">
+    <div
+      className="container-xl flex flex-col items-center py-10"
+      id="receitas"
+    >
       <h1 className="text-verde mb-4 text-center text-4xl font-semibold md:text-6xl">
         Receitas <span className="text-orange">saudáveis</span> especialmente
         para você
@@ -20,6 +23,7 @@ const Receitas = () => {
         estilo={
           "w-[300px] h-[50px] rounded-full bg-orange-400 px-8 py-2 font-semibold text-white botao-laranja"
         }
+        funcao={undefined}
       />
     </div>
   );
